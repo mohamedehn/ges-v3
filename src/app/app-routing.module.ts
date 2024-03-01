fef23953fcb6f29a4a2511e3e054e588506fa5e5
mohamedehn <mohamed.ebarhmatin@yahoo.fr>
2024-03-01 21:43:02 +0100

    start home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full'},
+  // { path: '', redirectTo: '/home', pathMatch: 'full' },
+  // { path: 'pv' },
+  // { path: 'climatisation' },
+  // { path: 'chauffage' },
+  // { path: 'professionnels' },
+  // { path: 'aides' },
+  // { path: 'contact'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
