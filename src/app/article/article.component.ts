15f20cc6830770109bb5f3ad6a1c71fbe79035ff
mohamedehn <mohamed.ebarhmatin@yahoo.fr>
2024-03-04 23:26:58 +0100

    article compo ok, starting to deploy articles,  need fix css & reponsive

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.scss']
 })
-export class ArticleComponent {
+export class ArticleComponent implements OnInit{
+  @Input() title!: string;
+  @Input() content!: string;
+  @Input() textBtn!: string;
+  @Input() image!: string;
 
+  constructor() { }
+
+  ngOnInit(): void {
+    this.title = this.title;
+    this.content = this.content;
+    this.image = this.image;
+    this.textBtn = this.textBtn;
+  }
 }
