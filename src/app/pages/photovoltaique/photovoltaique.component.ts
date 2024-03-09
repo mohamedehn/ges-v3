9fbe862c0ea68f5e0ec5f026a32931561cf65c05
mohamedehn <mohamed.ebarhmatin@yahoo.fr>
2024-03-09 23:55:32 +0100

    more content and assets

--- src/app/pages/photovoltaique/photovoltaique.component.ts
+++ src/app/pages/photovoltaique/photovoltaique.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-photovoltaique',
   templateUrl: './photovoltaique.component.html',
   styleUrls: ['./photovoltaique.component.scss']
 })
-export class PhotovoltaiqueComponent {
+export class PhotovoltaiqueComponent implements OnInit{
+
+  constructor() { }
+
+  ngOnInit(): void {
+    window.scrollTo(0, 0);
+  }
 
 }
